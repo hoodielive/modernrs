13da152b3d4774c6db199896ff418a209a6cec9a
hoodielive <hoodielive@gmail.com>
2022-05-12 23:14:16 -0400

    Damn this is good.

--- 2022/learn/src/main.rs
+++ 2022/learn/src/main.rs
@@ -1,15 +1,18 @@
 fn main() {
 
-    let abool: bool = true;
+    let _abool: bool = true;
 
-    let a_float: f64 = 1.0;
-    let an_integer = 5i32;
-    let an_int2: i32 = 5;
+    let _a_float: f64 = 1.0;
+    let _an_integer = 5i32;
+    let _an_int2: i32 = 5;
 
-    let default_float = 3.0;
-    let default_integer = 7;
+    let _default_float = 3.0;
+    let _default_integer = 7;
 
-    println!("The integer is {}", an_int2);
-    println!("The integer is {}", an_integer);
+    println!("The integer is {}", _an_int2);
+    println!("The integer is {}", _an_integer);
+
+    let mut _mutable = 12; // i32
+    _mutable = 21;
 
 }
