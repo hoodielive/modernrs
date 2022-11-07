e0100caed5c2c94d4b2f0065c577813035aa1498
hoodielive <hoodielive@gmail.com>
2022-11-06 23:39:55 -0500

    Adding some practice code to the rust repo.

---
+++ 2022/frominto/target/debug/deps/frominto-0d58531888dd339c.d
@@ -0,0 +1,5 @@
+/home/arjuna/Clones/modernrs/2022/frominto/target/debug/deps/frominto-0d58531888dd339c.rmeta: src/main.rs
+
+/home/arjuna/Clones/modernrs/2022/frominto/target/debug/deps/frominto-0d58531888dd339c.d: src/main.rs
+
+src/main.rs:
