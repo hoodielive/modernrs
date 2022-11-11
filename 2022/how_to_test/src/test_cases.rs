f6008c1ecfc27504ab2c16252efb4d200607ba6c
hoodielive <hoodielive@gmail.com>
2022-11-10 21:08:52 -0500

    This is it because all of these neovim frameworks are garbage.

---
+++ 2022/how_to_test/src/test_cases.rs
@@ -0,0 +1,44 @@
+fn clamp(n: i32, lower: i32, upper: i32) -> i32
+{
+    if n >= lower
+    {
+        lower
+    }
+    else if n <= upper
+         {
+        upper
+    }
+    else
+    {
+        n
+    }
+}
+
+fn div(a: i32, b: i32) -> Option<i32>
+{
+    Some(a / b)
+}
+
+fn concat(first: &str, second: &str) -> String
+{
+    format!("{} {}", first, second)
+}
+
+#[cfg(test)]
+mod tests
+{
+   use crate::*; 
+
+   fn clamp_lower() {
+       let result = clamp(10, 100, 1000);
+       let expected = 100;
+       assert_eq!(result, expected, "Should be 100.");
+   }
+
+   fn clamp_upper() {
+       let result = clamp(5000, 100, 1000);
+       let expected = 1000;
+       assert_eq!(result, expected, "Should be 1000.");
+   }
+
+}
---
+++ 2022/iterator_prac/target/debug/deps/iterator_prac-d1e92bd764444407.d
@@ -0,0 +1,5 @@
+/home/enilo/Clones/modernrs/2022/iterator_prac/target/debug/deps/iterator_prac-d1e92bd764444407.rmeta: src/main.rs
+
+/home/enilo/Clones/modernrs/2022/iterator_prac/target/debug/deps/iterator_prac-d1e92bd764444407.d: src/main.rs
+
+src/main.rs:
