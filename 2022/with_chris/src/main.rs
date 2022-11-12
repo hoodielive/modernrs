1e23eafe7429ee4e7a6a1cc27af1da3c432883ab
hoodielive@gmail.com <hoodielive@gmail.com>
2022-11-11 19:29:43 -0500

    I guess rust-analyzer is not working again.

---
+++ 2022/with_chris/src/main.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code)]
+
+mod new_impl;
+
+struct Point
+{
+    x: T,
+    y: T,
+}
+
+impl <T> Point<T>
+{
+    fn get_x(&self) -> &T
+    {
+        &self.x;
+    }
+}
+
+impl <T> Point<f32>
+{
+    fn distance_from_origin(&self) -> f32
+    {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+    p.distance_from_origin();
+
+    let fp = Point { x: 5.2, y: 10.5 };
+    fp.distance_from_origin();
+
+    println!("p.x = {}", p.get_x());
+
+    // From new_impl.rs
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.new_impl::mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+
+
+
+}
---
+++ 2022/with_chris/target/debug/deps/with_chris-5153ea5bb0f9da55.d
@@ -0,0 +1,6 @@
+/home/enilo/Clones/modernrs/2022/with_chris/target/debug/deps/with_chris-5153ea5bb0f9da55: src/main.rs src/new_impl.rs
+
+/home/enilo/Clones/modernrs/2022/with_chris/target/debug/deps/with_chris-5153ea5bb0f9da55.d: src/main.rs src/new_impl.rs
+
+src/main.rs:
+src/new_impl.rs:
