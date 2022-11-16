0ef9b44fe7711fdfc591873d40c1dfeca7758f99
hoodielive <hoodielive@gmail.com>
2022-11-16 17:20:59 -0500

    Branching

---
+++ 2022/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/enilo/Clones/modernrs/2022/branches/target/debug/branches: /home/enilo/Clones/modernrs/2022/branches/src/main.rs
---
+++ 2022/branches/target/debug/deps/branches-f05f12b36315bb26.d
@@ -0,0 +1,5 @@
+/home/enilo/Clones/modernrs/2022/branches/target/debug/deps/branches-f05f12b36315bb26.rmeta: src/main.rs
+
+/home/enilo/Clones/modernrs/2022/branches/target/debug/deps/branches-f05f12b36315bb26.d: src/main.rs
+
+src/main.rs:
