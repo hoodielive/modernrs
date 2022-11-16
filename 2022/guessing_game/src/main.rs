3e59bde8a7971c19f051d3dcfb9c0a5930392260
hoodielive <hoodielive@gmail.com>
2022-11-15 22:16:53 -0500

    Don't dwell in the past.

--- 2022/guessing_game/src/main.rs
+++ 2022/guessing_game/src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main()
 {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Your secret number is: {secret_number}");
@@ -17,5 +18,17 @@ fn main()
         .read_line(&mut guess)
         .expect("Failed to read line.");
 
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number.");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number)
+    {
+        Ordering::Less => println!("Too small."),
+        Ordering::Greater => println!("Too big."),
+        Ordering::Equal => println("You win!"),
+    }
 }
