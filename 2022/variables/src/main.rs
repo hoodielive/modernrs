46db305cdf67d79d9e49118cd7e42ae3feb19084
hoodielive <hoodielive@gmail.com>
2022-11-16 00:05:42 -0500

    Okay, good night

--- 2022/variables/src/main.rs
+++ 2022/variables/src/main.rs
@@ -1,3 +1,14 @@
+const A_VAL: i32 = 30 * 30 * 30;
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    // shadowing
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("Inner: The value of x in the inner scope is: {x}");
+    }
+
+    println!("Outer: The value of x in the inner scope is: {x}");
 }
