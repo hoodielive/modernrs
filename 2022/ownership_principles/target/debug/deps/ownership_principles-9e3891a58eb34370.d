d00898e7bf85d5daecf103a8fa5a769f725b088e
hoodielive <hoodielive@gmail.com>
2022-11-16 20:30:25 -0500

    Ownership is fascinating.

---
+++ 2022/ownership_principles/target/debug/deps/ownership_principles-9e3891a58eb34370.d
@@ -0,0 +1,5 @@
+/home/enilo/Clones/modernrs/2022/ownership_principles/target/debug/deps/ownership_principles-9e3891a58eb34370.rmeta: src/main.rs
+
+/home/enilo/Clones/modernrs/2022/ownership_principles/target/debug/deps/ownership_principles-9e3891a58eb34370.d: src/main.rs
+
+src/main.rs:
