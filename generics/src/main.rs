ef8d0b5e620972280104d32b4d04d0751c3d7171
hoodielive <hoodielive@gmail.com>
2022-11-16 19:43:39 -0500

    Important stop here.

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -74,3 +74,8 @@ fn call_overview_simplified<T: Overview>(item: &T)
 {
     println!("Overview: {}", item.overview())
 }
+
+fn overview_02(item1: &impl Overview, item2: &impl Overview) {};
+fn overview_03<T: Overview>(item1: &T, item2: &T) {};
+fn overview_04(item1: &impl Overview + AnotherTrait) {};
+fn overview_05<T: Overview + AnotherTrait>(item1: &T, item2: &T) {};
