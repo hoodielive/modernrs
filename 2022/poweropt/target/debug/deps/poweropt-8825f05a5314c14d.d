19fcb80472264b0bf4aff00b065383b0c1be1f8d
hoodielive <hoodielive@gmail.com>
2022-11-18 23:06:07 -0500

    tired

---
+++ 2022/poweropt/target/debug/deps/poweropt-8825f05a5314c14d.d
@@ -0,0 +1,5 @@
+/home/enilo/Clones/modernrs/2022/poweropt/target/debug/deps/poweropt-8825f05a5314c14d.rmeta: src/main.rs
+
+/home/enilo/Clones/modernrs/2022/poweropt/target/debug/deps/poweropt-8825f05a5314c14d.d: src/main.rs
+
+src/main.rs:
