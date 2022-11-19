f251a4fa7429e21a12a2748d66e6c776090e20d9
hoodielive <hoodielive@gmail.com>
2022-11-18 20:17:52 -0500

    sigh

---
+++ 2022/ext_modules/lib/mathematics.rs
@@ -0,0 +1,12 @@
+
+fn add(lhs: isize, rhs: isize) -> isize {
+    lhs + rhs
+}
+
+fn sub(lhs: isize, rhs: isize) -> isize {
+    lhs - rhs
+}
+
+fn mul(lhs: isize, rhs: isize) -> isize {
+    lhs * rhs
+}
