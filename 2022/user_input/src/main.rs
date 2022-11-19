e40eea10c09d23af9574c625e90cd752f903906c
hoodielive <hoodielive@gmail.com>
2022-11-18 22:20:16 -0500

    Roasted.

--- 2022/user_input/src/main.rs
+++ 2022/user_input/src/main.rs
@@ -3,6 +3,7 @@
 #![allow(unused_imports)]
 
 use std::io;
+mod power;
 
 fn get_input() -> io::Result<String>
 {
@@ -20,7 +21,8 @@ fn main()
    {
        match get_input()
        {
-           Ok(words) => {
+           Ok(words) => 
+           {
                 all_input.push(words);
                 times_input += 1;
            },
@@ -35,5 +37,27 @@ fn main()
            input,
            input.to_uppercase()
        );
+   }
+    
+   let mut user_input_for_power = vec![];
+   let mut times_input_power = 0;
+
+   while times_input_power < 2
+   {
+        match power::power_options(String::from("Off"))
+        {
+            Ok(selection) => 
+            {
+                user_input_for_power.push(selection);
+                times_input_power += 1;
+            }
+            Err(e) => println!("You have made the following error: {:?}", e),
+            
+        }
+   }
+
+   for input_power in user_input_for_power
+   {
+       println!("You have selected: {:?}", input_power);
    }
 }
--- 2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.d
+++ 2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.d
@@ -1,5 +1,6 @@
-/home/enilo/Clones/modernrs/2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.rmeta: src/main.rs
+/home/enilo/Clones/modernrs/2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.rmeta: src/main.rs src/power.rs
 
-/home/enilo/Clones/modernrs/2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.d: src/main.rs
+/home/enilo/Clones/modernrs/2022/user_input/target/debug/deps/user_input-9ffca295c0e3120d.d: src/main.rs src/power.rs
 
 src/main.rs:
+src/power.rs:
