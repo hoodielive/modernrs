e414f2525cdd8bf9bb7d593fdf4f4cde2add095d
hoodielive <hoodielive@gmail.com>
2022-11-30 23:16:09 -0500

    Thank you, gn

---
+++ 2022/vectoria/src/main.rs
@@ -0,0 +1,14 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
+fn main()
+{
+   let mut v: Vec<i32> = Vec::new();
+   v.push(1);
+   v.push(2);
+   v.push(3);
+   v.push(4);
+
+   println!("Time {:?}", &v);
+}
