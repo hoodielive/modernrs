4643c05239d48d03920ba6f044c3e3e88c877037
hoodielive <hoodielive@gmail.com>
2022-12-05 23:36:32 -0500

    sign out for tonight

--- 2022/traits_types_lifetimes/src/main.rs
+++ 2022/traits_types_lifetimes/src/main.rs
@@ -17,15 +17,10 @@ fn main()
 {
    let number_list = vec![34, 50, 25, 100, 65];
 
-   let mut largest = &number_list[0];
+   let result = largest(&number_list);
+   println!("The largest number is {}.", result);
 
-   for number in &number_list
-   {
-       if number > largest
-       {
-           largest = number;
-       }
-   }
-
-   println!("The largest number is {}.", largest);
+   let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+   let result = largest(&number_list);
+   println!("The largest number is {}.", result);
 }
