4e5f7245b8cbe592704a7641a30c300542c387ae
hoodielive <hoodielive@gmail.com>
2022-12-08 22:32:12 -0500

    Use a generic instead.

--- 2022/traits_types_lifetimes/src/main.rs
+++ 2022/traits_types_lifetimes/src/main.rs
@@ -1,26 +1,25 @@
-fn largest(list: &[i32]) -> &i32
+#![allow(dead_code)]
+
+fn largest_generic<T>(list: &[T]) -> &T
 {
-   let mut largest = &list[0];
+    let mut largest = &list[0];
 
-   for item in list
-   {
-       if item > largest
-       {
-           largest = item;
-       }
-   }
+    for item in list
+    {
+        largest = item;
+    }
 
-   largest
+    largest
 }
 
 fn main() 
 {
    let number_list = vec![34, 50, 25, 100, 65];
 
-   let result = largest(&number_list);
+   let result = largest_generic(&number_list);
    println!("The largest number is {}.", result);
 
    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-   let result = largest(&number_list);
+   let result = largest_generic(&number_list);
    println!("The largest number is {}.", result);
 }
