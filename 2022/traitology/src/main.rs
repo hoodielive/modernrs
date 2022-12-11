4c53993c414cdde4530b044658c73b2ee34730a7
hoodielive <hoodielive@gmail.com>
2022-12-11 00:38:55 -0500

    Traits are like interfaces but are not the same in all ways

---
+++ 2022/traitology/src/main.rs
@@ -0,0 +1,64 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
+pub struct NewsArticle
+{
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+pub trait Summary
+{
+    fn summarize(&self) -> String;
+}
+
+impl Summary for NewsArticle
+{
+    fn summarize(&self) -> String
+    {
+        format!
+        (
+            "{}, by {} ({}).", 
+            self.headline, 
+            self.author, 
+            self.location,
+        )
+    }
+}
+
+pub struct Tweet
+{
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet
+{
+    fn summarize(&self) -> String
+    {
+        format!
+        (
+            "{}: {}",
+            self.username,
+            self.content
+        )
+    }
+}
+
+fn main()
+{
+    let tweet = Tweet
+    {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably know"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
