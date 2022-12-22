3d72eec06d7dbac1e14d68376af3e1d9e60ae614
hoodielive <hoodielive@gmail.com>
2022-12-21 23:49:14 -0500

    Agenda

--- 2022/exp/src/main.rs
+++ 2022/exp/src/main.rs
@@ -1,12 +1,27 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 struct Fruit
 {
-    apples: i32,
-    bananas: i32,
+    apples: u32,
+    bananas: u32,
+}
+
+struct FruitString
+{
+    apples: String,
+    bananas: String,
+}
+
+struct Fruity<T>
+{
+    apples: T,
+    bananas: T,
 }
 
 impl Fruit
 {
-    fn price(self) -> i32
+fn price(self) -> u32
     {
         self.apples * 8 + self.bananas * 12
     }
@@ -26,4 +41,13 @@ fn main()
    let fruit = Fruit::new_fruit();
    let price = fruit.price();
    println!("Price is {}", price);
+
+   let x = 5;
+
+   let _fruit32: Fruit<u32> = Fruit
+   {
+       apples: 5_u32,
+       bananas: 10_u32,
+   };
+   
 }
