fc5515f0b6d3a4a972e4c6b0ac65b7de5d57c088
hoodielive <hoodielive@gmail.com>
2022-12-29 11:53:09 -0500

    What time is it

--- 2023/actix-project/src/main.rs
+++ 2023/actix-project/src/main.rs
@@ -5,16 +5,22 @@ struct AppState
     app_name: String,
 }
 
-async fn index() -> impl Responder
+async fn index3() -> impl Responder
 {
     "Oyeku says Hello!"
 }
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> impl String
+async fn index(data: web::Data<AppState>) -> String
 {
     let app_name = &data.app_name;
     format!("Hello {app_name}!")
+}
+
+async fn index2(data: web::Data<AppState>) -> impl String
+{
+    let app_name = &data.app_name;
+    format!("Hello {app_name}")
 }
 
 #[post("/echo")]
@@ -33,10 +39,18 @@ async fn main() -> std::io::Result<()>
 {
     HttpServer::new(|| {
         App::new()
+            .app_data(
+                web::Data::new(
+                    AppState {
+                        app_name: String::from("Oyeku Meji is in the building!"),
+                    }
+                )
+            )
+            .service(index)
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
-            .route("/index.html", web::get().to(index))
+            .route("/index.html", web::get().to(index3))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
