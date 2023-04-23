34919692bdad178d63fd2b2fd44d1e8ea9e8c168
hoodielive <hoodielive@gmail.com>
2023-04-22 23:11:43 -0400

    Brooms.

--- 2023/struct-ca/src/main.rs
+++ 2023/struct-ca/src/main.rs
@@ -1,9 +1,21 @@
 #[allow(dead_code)]
-struct GrayscaleMap
+pub struct GrayscaleMap
 {
-    pixels: Vec<u8>,
-    size: (usize, usize),
+    pub pixels: Vec<u8>,
+    pub size: (usize, usize),
 }
+
+struct broom
+{
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+#[derive(Copy, Clone)]
+enum BroomIntent { FetchWater, DumpWater }
 
 fn main() 
 {
@@ -22,4 +34,11 @@ fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap
 {
     assert_eq!(pixels.len(), size.0 * size.1);
     GrayscaleMap { pixels, size }
+}
+
+fn chop(b: Broom) -> (Broom, Broom)
+{
+    let mut broom1 = Broom { height: b.height / 2, .. b};
+    let mut broom2 = Broom { name: broom1.name.clone() /  .. broom1 };
+    
 }
