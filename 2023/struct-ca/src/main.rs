ac465ecfbdaa03a9df23f27f5871c7d35b3c4744
hoodielive <hoodielive@gmail.com>
2023-04-23 12:44:37 -0400

    Broom expanded.

--- 2023/struct-ca/src/main.rs
+++ 2023/struct-ca/src/main.rs
@@ -5,7 +5,7 @@ pub struct GrayscaleMap
     pub size: (usize, usize),
 }
 
-struct broom
+struct Broom
 {
     name: String,
     height: u32,
@@ -28,6 +28,17 @@ fn main()
 
     assert_eq!(image.size, (1024, 576));
     assert_eq!(image.pixels.len(), 1024 * 576);
+
+    let hokey = Broom {
+        name: "Hokey".to_string(),
+        height: 60,
+        health: 100,
+        position: (100.0, 200.0, 0.0),
+        intent: BroomIntent::FetchWater,
+    };
+
+    let (hokey1, hokey2) = chop(hokey);
+    assert_eq!(hokey1.name, "Hokey I");
 }
 
 fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap
@@ -39,6 +50,10 @@ fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap
 fn chop(b: Broom) -> (Broom, Broom)
 {
     let mut broom1 = Broom { height: b.height / 2, .. b};
-    let mut broom2 = Broom { name: broom1.name.clone() /  .. broom1 };
-    
+    let mut broom2 = Broom { name: broom1.name.clone(), .. broom1 };
+
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
 }
