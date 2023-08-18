9f7fb2f3caa65307f8a87b8642247874c25b2cd1
hoodielive <hoodielive@gmail.com>
2023-08-17 20:08:35 -0400

    Generics are abstract standins for concrete types.

---
+++ 2023/bears/src/main.rs
@@ -0,0 +1,19 @@
+// Use generics.
+// Generics are abstract stand-ins for concrete types or other properties.
+
+fn main() 
+{
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut largest = &number_list[0];
+
+    for number in &number_list 
+    {
+        if number > largest 
+        {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+}
