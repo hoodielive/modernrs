49140bc5fba54e36e8d1be4665e57fb85eefedff
hoodielive <hoodielive@gmail.com>
2023-10-24 20:30:59 -0400

    Don't argue. It sucks

--- 2023/astrology_proto/src/astrology.rs
+++ 2023/astrology_proto/src/astrology.rs
@@ -1,1 +1,26 @@
-use 
+use actix_web::{get, web, HttpResponse, Responder};
+use reqwest;
+use serde_json::Value;
+
+#[get("/horoscope/{sign}")]
+async fn horoscope(sign: web::Path<String>) -> impl Responder {
+    let url = format!("https://astrology-api.example.com/horoscope?sign={}", sign);
+
+    let response = reqwest::get(&url).await;
+
+    match response {
+        Ok(resp) => {
+            let horoscope: Result<Value, _> = resp.json().await;
+            match horoscope {
+                Ok(horoscope_data) => HttpResponse::Ok().json(horoscope_data),
+                Err(e) => HttpResponse::InternalServerError().body(format!("Failed to parse JSON: {}", e)),
+            }
+        }
+        Err(e) => HttpResponse::InternalServerError().body(format!("Failed to fetch from API: {}", e)),
+    }
+}
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(horoscope);
+}
+
--- 2023/astrology_proto/src/main.rs
+++ 2023/astrology_proto/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod astrology;
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/").configure(astrology::init_routes))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
