feafa622e83ff5445b486cbf244388f2a3917dd8
hoodielive <hoodielive@gmail.com>
2024-01-07 08:41:45 -0500

    Check in

--- 2024/processes/src/main.rs
+++ 2024/processes/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+// Load Module.
+mod mytypes;
+
+// Introduce the Color Type into scope.
+use mytypes::Color;
+
+fn main() 
+{
+    demo_simple_enums()
+    
+}
+
+fn demo_simple_enums()
+{
+    println!("Enums...");
+    let c: Color = Color::Red;
+
+    match c {
+        Color::Red   => println!("Coch."),
+        Color::Green => println!("Gwyrdd."),
+        Color::Blue  => println!("Glas."),
+    }
 }
---
+++ 2024/processes/src/mytypes.rs
@@ -0,0 +1,20 @@
+#[allow(dead_code)]
+pub enum Flight{
+   Economy,
+   Business,
+   First,
+}
+
+#[allow(dead_code)]
+pub enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+#[allow(dead_code)]
+pub enum HouseLocation {
+    Number(i32),
+    Name(String),
+    Unknown
+} 
