c324a5b130a40fc1ec0e61ab98413acd36382fa3
hoodielive <hoodielive@gmail.com>
2024-07-07 12:31:32 -0400

    Ancestor.

---
+++ 2024/activities/src/bin/determine.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code)]
+fn main()
+{
+    
+}
+
+enum Orisa
+{
+    Warfare,
+    Manipulation,
+    InstantaneousHealing,
+    Seduction,
+    Prowess,
+    MindControl
+}
+
+enum Temp
+{
+    Hot,
+    Cold
+}
+
+
