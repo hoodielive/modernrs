a78638dace3a0df5b5612b5bce4e624e2f46165b
hoodielive <hoodielive@gmail.com>
2025-11-06 20:47:53 -0500

    100 games.

--- 2025/calculator/src/main.rs
+++ 2025/calculator/src/main.rs
@@ -1,6 +1,29 @@
-fn main() 
+use std::any::type_name;
+
+// typeOf implementation
+fn type_of<T>(_:&T) -> &'static str
+{
+    type_name::<T>()
+}
+
+fn speed(start: u32, end: u32, time_elasped: u32) -> u32 
+{
+    let distance = end - start;
+    distance / time_elasped
+}
+
+fn main()
 {
   let b: u8 = 100;
   let a: u32 = b.into();
   println!("This is b: {} and this is a: {}", b, a);
+
+  let _x = 42; 
+  let _y: u32 = _x;
+
+  println!("The type of x is: {}", type_of(&_x));
+  let _speed = speed(30, 90, 4);
+  println!("{} is what all that balls down to.", _speed);
+
+  panic!("This is a panic on purpose!");
 }
