5b0076e734875200620f0011fc27acb72ae7fa3c
hoodielive <hoodielive@gmail.com>
2025-11-22 21:51:36 -0500

    Validation tests now pass.

---
+++ 2025/ticket/src/lib.rs
@@ -0,0 +1,21 @@
+struct Thing
+{
+    field: i32,
+}
+
+enum Choice
+{
+    A,
+    B,
+    C
+}
+
+fn main()
+{
+    let _corre = Thing 
+    {
+        field: "A"
+    };
+    
+    println!("Hello, World!");
+}
